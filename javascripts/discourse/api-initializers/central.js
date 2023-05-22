bbbc8b45f5eb0d10f65f2f6dca53c74c94f0c498
Carson Chang <carsick@yahoo.com>
2023-05-21 19:30:27 -0700

    init

--- javascripts/discourse/api-initializers/central.js
+++ javascripts/discourse/api-initializers/central.js
@@ -3,11 +3,18 @@ import { apiInitializer } from "discourse/lib/api"
 export default apiInitializer("0.11.1", (api) => {
   api.onPageChange(() => {
     const activePage = window.location.href
+    const homePage = window.origin + "/"
     const links = document
       .querySelectorAll(".sidebar-left__link a")
       .forEach((link) => {
         link.classList.remove("active")
-        if (link.href === activePage) {
+        if (
+          link.href !== homePage &&
+          activePage !== homePage &&
+          activePage.includes(link.href)
+        ) {
+          link.classList.add("active")
+        } else if (link.href == activePage) {
           link.classList.add("active")
         }
       })
