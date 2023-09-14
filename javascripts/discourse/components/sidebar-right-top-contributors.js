705453bc7c03e7a5d5484befcd15d1f536b4108e
Carson Chang <carsick@yahoo.com>
2023-09-14 15:15:38 -0700

    category icons

---
+++ javascripts/discourse/components/sidebar-right-top-contributors.js
@@ -0,0 +1,23 @@
+import Component from "@glimmer/component"
+import { ajax } from "discourse/lib/ajax"
+import { tracked } from "@glimmer/tracking"
+
+export default class SidebarRightTopContributors extends Component {
+  @tracked topContributors = null
+
+  constructor() {
+    super(...arguments)
+
+    const count = this.args?.params?.count || 5
+
+    ajax(`/directory_items.json?period=yearly&order=likes_received`).then(
+      (data) => {
+        this.topContributors = data.directory_items.slice(0, count)
+      }
+    )
+  }
+
+  willDestroy() {
+    this.topContributors = null
+  }
+}
