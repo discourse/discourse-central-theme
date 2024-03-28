07b5b9a4cb8fa839fd4be17727ef4b321e922f34
Jarek Radosz <jradosz@gmail.com>
2024-03-28 11:19:10 +0100

    DEV: Update linting

--- javascripts/discourse/connectors/above-main-container/sidebar-right.js
+++ javascripts/discourse/connectors/above-main-container/sidebar-right.js
@@ -1,5 +1,5 @@
 import Component from "@glimmer/component";
-import { inject as service } from "@ember/service";
+import { service } from "@ember/service";
 import { defaultHomepage } from "discourse/lib/utilities";
 
 export default class SidebarRight extends Component {
