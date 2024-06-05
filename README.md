dc4c46df9ae399ed42bb306d063392dccc8a7458
carsick <carsick@yahoo.com>
2024-06-05 10:36:07 -0700

    refactor

--- README.md
+++
@@ -1,5 +0,0 @@
-# Central
-
-**Theme Summary**
-
-For more information, please see: **url to meta topic**
--- javascripts/discourse/helpers/ends-with-ellipsis.js
+++
@@ -1,3 +0,0 @@
-export default function endsWithEllipsis(excerpt) {
-  return excerpt.endsWith("&hellip;");
-}
